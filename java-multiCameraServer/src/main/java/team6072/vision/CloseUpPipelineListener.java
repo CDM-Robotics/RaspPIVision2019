1f36a5b8372dc926f101ca9e2df1fb0d9938b1df
austin leehealey <austinleehealey@gmail.com>
2019-02-10 12:28:00 -0800

    Hello! New update to stuff First I changed the code so that it is MUCH easier to read and understand hopefully. I seperated things into seperate functions and made things much mmore logical for people to decipher. Then I also added a find distance from target function that will work even if the camera is off horiantally, but requires that it is perpendicular vertically, which I believe is a given with the robot design. So that should be adequite enough for our purposes. However, there are some problems and 1 of them is that the FOV vertical angle for the camera in the code is still wrong, and I need to fix it. And Also I now need to use that vertical distance function to find the angle that the camera is distorted, however, that only will be possible after i tune the vertical FOV for the camera to be correct.

--- java-multiCameraServer/src/main/java/team6072/vision/CloseUpPipelineListener.java
+++ java-multiCameraServer/src/main/java/team6072/vision/CloseUpPipelineListener.java
@@ -15,6 +15,8 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.imgproc.Imgproc;
 
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
+
 import java.util.ArrayList;
 import org.opencv.core.Mat;
 
@@ -37,8 +39,11 @@ public class CloseUpPipelineListener implements VisionRunner.Listener<CloseUpPip
     private NetworkTableEntry mY2;
 
     private final double TAPE_DIST_FROM_CENTER_INCHES_X = 5.65572;
-    private final double HALF_OF_CAMERA_FOV_ANGLE_X = 0.9098492;
-    private final int HALF_OF_CAMERA_PIXEL_WIDTH_PIXELS = 80;    
+    private final double TAPE_HEIGHT_IN_INCHES = 5.825352102;
+    private final double CAMERA_FOV_ANGLE_X = 0.9098492;
+    private final double CAMERA_FOV_ANGLE_Y = 1.028149899; // fix this bc its probably wrong
+    private final int CAMERA_PIXEL_WIDTH_PIXELS = 160;
+    private final int CAMERA_PIXEL_HEIGHT_PIXELS = 120;
 
     // Pipelines
     private CloseUpPipeline mPipeline;
@@ -51,13 +56,6 @@ public class CloseUpPipelineListener implements VisionRunner.Listener<CloseUpPip
     // Counters
     private int mCallCounter = 0;
     private int mCounter;
-
-    public double abs(double num) {
-        if (num < 0) {
-            num = num * -1;
-        }
-        return num;
-    }
 
     public CloseUpPipelineListener() {
         // instantiate Network Tables
@@ -98,7 +96,7 @@ public class CloseUpPipelineListener implements VisionRunner.Listener<CloseUpPip
                 outputColorFilters();
                 // rectangles output
                 ArrayList<MatOfPoint> mats = mPipeline.findContoursOutput();
-                ArrayList<RotatedRect> rectangles = new ArrayList<RotatedRect>();
+                ArrayList<RotatedRect> rotatedRects = new ArrayList<RotatedRect>();
                 // Network Tables Stuff
                 mTbl.getEntry("PIKey").setString("Call: " + mCounter);
                 if (mats.size() == 2) {
@@ -108,35 +106,20 @@ public class CloseUpPipelineListener implements VisionRunner.Listener<CloseUpPip
                         Point center = rect.center;
                         double angle = rect.angle;
                         Size size = rect.size;
+
                         mTbl.getEntry("Rect " + i + " center X").setString("x = " + center.x);
                         mTbl.getEntry("Rect " + i + " center Y").setString("y = " + center.y);
                         mTbl.getEntry("Rect " + i + " angle").setDouble(angle);
                         mTbl.getEntry("Rect " + i + " Size").setString("Size : " + (size.width * size.height));
 
-
-                        rectangles.add(rect);
+                        rotatedRects.add(rect);
                     }
-                    //Calculate the center of mass of the tape
-                    double mass1 = rectangles.get(0).size.height * rectangles.get(0).size.width;
-                    double mass2 = rectangles.get(1).size.height * rectangles.get(1).size.width;
-                    double massCenterXpx = ((mass1 * rectangles.get(0).center.x + mass2 * rectangles.get(1).center.x)
-                            / (mass1 + mass2));
-                    double massCenterYpx = ((mass1 * rectangles.get(0).center.y + mass2 * rectangles.get(1).center.y)
-                            / (mass1 + mass2));
 
-                    mTbl.getEntry("Center Of Mass X").setDouble(massCenterXpx);
-                    mTbl.getEntry("Center Of Mass Y").setDouble(massCenterYpx);
-                    //Calculate the distance from the target X horizontally
-                    double tapeDistFromCenterPxX = abs(rectangles.get(0).center.x - massCenterXpx);
-                    double halfOfCameraPixelWidthInches = (TAPE_DIST_FROM_CENTER_INCHES_X / tapeDistFromCenterPxX) * HALF_OF_CAMERA_PIXEL_WIDTH_PIXELS;
-                    double distanceFromTargetX = halfOfCameraPixelWidthInches / (java.lang.Math.tan( HALF_OF_CAMERA_FOV_ANGLE_X / 2 ));
-                    
-                    mTbl.getEntry("halfOfCameraPixelWidthInches").setDouble(halfOfCameraPixelWidthInches);
-                    mTbl.getEntry("Distance From Target X").setDouble(distanceFromTargetX);
-                    // calculate the average position without weight
-                    double averagePosition = (rectangles.get(0).center.x + rectangles.get(1).center.x) / 2;
-                    mTbl.getEntry("Average Position pixels").setDouble(averagePosition);
-                    
+                    rotatedRects = orderRectangles(rotatedRects);
+
+                    mTbl.getEntry("Distance From Target X").setDouble(getDistanceFromTargetUsingXAxis(rotatedRects));
+                    mTbl.getEntry("Distance From Tape 1 Inches").setDouble(getDistanceFromTargetUsingYAxis(rotatedRects.get(0)));
+                    mTbl.getEntry("Distance From Tape 2 Inches").setDouble(getDistanceFromTargetUsingYAxis(rotatedRects.get(1)));
 
                 }
 
@@ -147,4 +130,106 @@ public class CloseUpPipelineListener implements VisionRunner.Listener<CloseUpPip
         }
     }
 
+    /**
+     * Returns the same array list of 2 RotatedRectangles but ordered from left to right
+     * - this is so that subsequent calculations so longer have to worry about the ordering of the rectangles
+     * @param rotatedRects - an array list of the RotatedRectangles directly from the camera's contour output
+     * @return - an array list of Rotated Rectangles
+     */
+    private ArrayList<RotatedRect> orderRectangles(ArrayList<RotatedRect> rotatedRects)
+    {
+
+        RotatedRect rect1 = rotatedRects.get(0);
+        RotatedRect rect2 = rotatedRects.get(1);
+        if(rect1.center.x > rect2.center.x)
+        {
+            RotatedRect tempRect = rect2;
+            rect2 = rect1;
+            rect1 = tempRect;
+        }
+        ArrayList<RotatedRect> tempRects = new ArrayList<RotatedRect>();
+        tempRects.add(rect1);
+        tempRects.add(rect2);
+        return tempRects;
+    }
+    /**
+     * Calculates the center of mass between two objects of varying pixel mass
+     */
+    private double centerOfMass(double size1, double size2, double position1, double position2)
+    {
+        return (((size1 * position1) + (size2 * position2)) / (position1 + position2));
+    }
+
+    public double abs(double num) {
+        if (num < 0) {
+            num = num * -1;
+        }
+        return num;
+    }
+
+    /**
+     * This function finds the distance between the camera and the target, 
+     *  - assuming that the target is perfectly perpendicular to the target
+     *  - uses the horizantal axis ONLY, meaning it does not take into account vertical distortion
+     * 
+     * @param rotatedRects - the Array list of ordered Rotated Rectanlges
+     * @return - returns the distance inbetween the camera and the target
+     */
+    private double getDistanceFromTargetUsingXAxis(ArrayList<RotatedRect> rotatedRects)
+    {
+
+        // Calculate the center of mass of the tape
+        double mass1 = rotatedRects.get(0).size.height * rotatedRects.get(0).size.width;
+        double mass2 = rotatedRects.get(1).size.height * rotatedRects.get(1).size.width;
+
+        double massCenterXpx = centerOfMass(mass1, mass2, rotatedRects.get(0).center.x, rotatedRects.get(1).center.x);
+        double massCenterYpx = centerOfMass(mass1, mass2, rotatedRects.get(0).center.y, rotatedRects.get(1).center.y);
+        
+        // Calculate the distance from the target X horizontally
+        // using the horizantal numbers
+        // assuming it is perpendicular to the wall
+
+        double tapeDistFromCenterPxX = abs(rotatedRects.get(0).center.x - massCenterXpx);
+        double halfOfCameraPixelWidthInches = (TAPE_DIST_FROM_CENTER_INCHES_X / tapeDistFromCenterPxX)
+                * (CAMERA_PIXEL_WIDTH_PIXELS / 2);
+        double distanceFromTargetX = halfOfCameraPixelWidthInches
+                / (java.lang.Math.tan(CAMERA_FOV_ANGLE_X / 2));
+                
+        mTbl.getEntry("Center Of Mass X").setDouble(massCenterXpx);
+        mTbl.getEntry("Center Of Mass Y").setDouble(massCenterYpx);
+
+        return distanceFromTargetX;
+    }
+    /**
+     * 
+     * @param rect - the desired rotated rectangle
+     * @param angle - the angle the rectangle is tilted at
+     * @return
+     */
+    private double findHeight(RotatedRect rect)
+    {
+        if ((rect.size.height) > (rect.size.width)) {
+            return rect.size.height;
+        } else {
+            return rect.size.width;
+        }
+    }
+    /**
+     * Calculates the distance from target X horizantally
+     * - using the vertical Axis
+     * - assuming it is perpendicular to the wall
+     * @param rotatedRects
+     * @return
+     */
+    private double getDistanceFromTargetUsingYAxis(RotatedRect rect)
+    {
+        double tapeHeightPx = findHeight(rect);
+        
+        double angle = (tapeHeightPx / CAMERA_PIXEL_HEIGHT_PIXELS) * CAMERA_FOV_ANGLE_Y;
+        double pxToInches = (TAPE_HEIGHT_IN_INCHES / tapeHeightPx);
+        double distanceFromTapePx = (tapeHeightPx / 2) / java.lang.Math.tan(angle / 2);
+        double distanceFromTapeInches = distanceFromTapePx * pxToInches;
+
+        return distanceFromTapeInches;
+    }
 }
